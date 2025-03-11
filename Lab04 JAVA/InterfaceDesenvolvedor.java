c97b63e6570150c5e26a0493f7fb3a5d884d0483
BernardoSilvaAndrade <bernardo.102006@gmail.com>
2025-03-11 11:56:41 -0300

    Delete Lab04 JAVA directory

--- Lab04 JAVA/InterfaceDesenvolvedor.java
+++
@@ -1,5 +0,0 @@
-public interface InterfaceDesenvolvedor {
-    String getLinguagemPrincipal();
-
-    String projetosRealizados();
-}
--- Lab04 JAVA/InterfaceGerente.java
+++
@@ -1,5 +0,0 @@
-public interface InterfaceGerente {
-    int getNumeroDeEquipes();
-
-    double calcularBonus();
-}
